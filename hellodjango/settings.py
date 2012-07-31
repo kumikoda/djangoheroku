20b395dd46452a4b78b593c1172c0b4efa070c8c
Anson Chu <anson.ch@gmail.com>
2012-07-31 14:15:46 -0700

    added admin

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -112,7 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 	'web',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
 
+	url(r'^admin/', include(admin.site.urls)),
+	
 	url(r'^$', 'web.views.index'),
 	url(r'^party/$', 'web.views.detail'),
 	url(r'^party/(?P<party_id>\d+)/$', 'web.views.detail'),
