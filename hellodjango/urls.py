71a1aefc37b375b607244504974aa462bc2d1748
Anson Chu <anson.ch@gmail.com>
2012-07-31 14:10:46 -0700

    added urls

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+party_resource = PartyResource()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'web.views.index'),
-    # url(r'^hellodjango/', include('hellodjango.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^$', 'web.views.index'),
+	url(r'^party/$', 'web.views.detail'),
+	url(r'^party/(?P<party_id>\d+)/$', 'web.views.detail'),
+	url(r'^parties/$', 'web.views.parties'),
+	url(r'^sms/$', 'web.views.sms'),
+	url(r'^seat/$', 'web.views.seat'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+
 )
