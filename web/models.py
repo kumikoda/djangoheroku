84949793f815868f0d70d548305dcc4d5d8e1e65
Anson Chu <anson.ch@gmail.com>
2012-07-31 13:30:51 -0700

    added models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Party(models.Model):
+	name = models.CharField(max_length=200)
+	size = models.IntegerField()
+	number = models.IntegerField()
+	status = models.CharField(max_length=2, choices = {("w","waiting"), ("m","messaged"), ("s","seated")} )
+	
+	
